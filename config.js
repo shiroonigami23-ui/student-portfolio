321659de142f1f9af6fd9efdaacce0da7d2cecda
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-16 14:18:41 +0530

    Update and rename script.js to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const THEMES = {
+    'theme-space': 'Space',
+    'theme-day': 'Day',
+    'theme-night': 'Night',
+    'theme-warm': 'Warm',
+    'theme-forest': 'Forest',
+    'theme-ocean': 'Ocean',
+    'theme-mint': 'Mint',
+};
