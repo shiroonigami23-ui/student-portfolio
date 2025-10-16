1785a2e0d18d1aca1a16e6e8947ad30f004e5146
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-16 23:18:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyACW4N486R7JF4OeAyVOr20XBnEa7Gzcgg",
   authDomain: "portfolio-architect-pro.firebaseapp.com",
@@ -18,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Export the services the rest of the app needs
+export const auth = getAuth(app);
+export const db = getFirestore(app);
