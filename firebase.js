95a67858337d4710c1033c6843df7cb2f66018a3
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-16 23:13:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Add your own Firebase configuration from your Firebase project settings
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyACW4N486R7JF4OeAyVOr20XBnEa7Gzcgg",
+  authDomain: "portfolio-architect-pro.firebaseapp.com",
+  projectId: "portfolio-architect-pro",
+  storageBucket: "portfolio-architect-pro.firebasestorage.app",
+  messagingSenderId: "688628767153",
+  appId: "1:688628767153:web:d4334ccebf3b29c1c78590",
+  measurementId: "G-V2Y6JG5B0K"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
