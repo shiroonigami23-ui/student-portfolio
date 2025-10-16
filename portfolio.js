87bc60367332c94092fcf2ba69a1afad3efbdf65
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-17 04:26:16 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,9 @@
-
 import { serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 
 export function createPortfolio(formData) {
     return {
         ...formData,
+        isPublic: false, // Default to private
         createdAt: serverTimestamp(),
         lastModified: serverTimestamp(),
     };
